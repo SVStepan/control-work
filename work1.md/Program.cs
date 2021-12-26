b23fb27d2457e88da5e86ab91ce727b9b406e074
Stepan <stepan_smirnoff@mail.ru>
2021-12-26 19:05:40 +0300

    продолжение

--- work1.md/Program.cs
+++ work1.md/Program.cs
@@ -6,3 +6,47 @@
 // [2, -4, 6] -> [2, -4, 6]
 // [1, 3, 5] -> []
 
+void printArr(int[] array)
+{
+    for(int i = 0; i < array.Length; i++ )
+     {
+     Console.Write(array[i] + " ");    
+     }
+     Console.WriteLine();
+ }
+
+void SelectChet(int[] array)
+{
+    for(int i = 0; i < array.Length; i++ )
+    {
+        if (array[i] % 2 == 0)
+        {
+            Console.Write(array[i] +" ");
+        }
+    }
+}   
+
+// 1 вводи массива руками
+int[] array = { 2, 7, 9, 12, 13, 15, 16, 21, 84, 91, 75, 17, 28, 31 };
+
+printArr(array);
+SelectChet(array);
+
+Console.WriteLine();
+
+
+// 2 вывод массива с случайными цифрами
+
+void PrintRandArr(int[] array)
+{
+        for (int i = 0; i < array.Length; i++)
+        {
+           array[i] = new Random().Next(1, 30);
+           Console.Write(array[i] +" ");
+        }
+}
+int[] arr = new int[10];
+
+PrintRandArr(arr);
+printArr(arr);
+SelectChet(arr);
